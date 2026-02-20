2f329c7d4e16ba48a22eda2ae63cb4ece8e6bfc4
nikhilrautan <nikhilrautan1@gmail.com>
2026-02-20 19:55:34 +0530

    UPDATED

--- 08.LINKED LIST/3 Reverse, Middle , Rotate LL/code.cpp
+++ 08.LINKED LIST/3 Reverse, Middle , Rotate LL/code.cpp
@@ -81,9 +81,8 @@ ListNode *middleNode(ListNode *head)
     while (fast != NULL && fast->next != NULL)
     {
         slow = slow->next;
-        fast = fast->next->next;
+        fast = fast->next->next; 
     }
-
     return slow;
 }
 
