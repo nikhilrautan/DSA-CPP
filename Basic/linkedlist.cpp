36b861d1e7e71ad1828a796363b4b93672f03ad2
nikhilrautan <nikhilrautan1@gmail.com>
2026-02-25 23:54:29 +0530

    UPDATED

---
+++ Basic/linkedlist.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+// class Node
+// {
+// public:
+//     int data;
+//     Node* next;
+// };
+
+// int main()
+// {
+//     Node* head = new Node();
+//     head->data = 10;
+//     head->next = NULL;
+
+//     Node* first = new Node();
+//     first->data = 28;
+//     first->next = NULL;
+
+//     head->next = first;
+
+//     Node* temp = head;
+//     Node*temp = new Node();
+//     temp->data =17;
+//     temp->next =head;
+//     head = temp;
+
+//     while(temp != NULL)
+//     {
+//         cout << temp->data << " -> ";
+//         temp = temp->next;
+//     }
+
+//     cout << "NULL";
+
+
+
+//     return 0;
+// }
+
+// creating LL using Recursion
+
