6c58a805d2f6b62aeadb25e6e78ceb373c060ab4
nikhilrautan <nikhilrautan1@gmail.com>
2026-03-05 21:38:21 +0530

    UPDATED

---
+++ 09.STACK/2 Questions/Make_array_beautiful.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+//Make the array beautiful
+vector<int> makeBeautiful(vector<int> arr)
+{
+    stack<int> s;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (s.empty())
+        {
+            s.push(arr[i]);
+        }
+        else if (arr[i] >= 0)
+        {
+            if (s.top() >= 0)
+                s.push(arr[i]);
+            else
+                s.pop();
+        }
+        else
+        {
+            if (s.top() < 0)
+                s.push(arr[i]);
+            else
+                s.pop();
+        }
+    }
+
+    vector<int> ans(s.size());
+    int i = s.size() - 1;
+
+    while (!s.empty())
+    {
+        ans[i--] = s.top();
+        s.pop();
+    }
+
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {4, -2, -2, 3};
+
+    vector<int> result = makeBeautiful(arr);
+
+    cout << "Beautiful array: ";
+    for (int x : result)
+    {
+        cout << x << " ";
+    }
+
+    return 0;
+}
