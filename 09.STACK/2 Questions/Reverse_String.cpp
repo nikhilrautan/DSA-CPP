0f9b3930f644f051dc233482a11b473a6d3f84ae
nikhilrautan <nikhilrautan1@gmail.com>
2026-04-01 21:03:10 +0530

    Added Comments

--- 09.STACK/2 Questions/Reverse_String.cpp
+++ 09.STACK/2 Questions/Reverse_String.cpp
@@ -8,7 +8,7 @@ void reverseString(vector<char> &s)
     stack<char> st;
 
     // Push characters into stack
-    for (int i = 0; i < s.size(); i++)
+    for (int i = 0; i < s.size(); i++)//stack m push krenge saare elements array k.
     {
         st.push(s[i]);
     }
@@ -16,7 +16,7 @@ void reverseString(vector<char> &s)
     int i = 0;
 
     // Pop from stack and put back in vector
-    while (!st.empty())
+    while (!st.empty())//ab jo stack top m hai usko array m daalte jaenge to hume (Reversed Array) mil jaega.
     {
         s[i++] = st.top();
         st.pop();
