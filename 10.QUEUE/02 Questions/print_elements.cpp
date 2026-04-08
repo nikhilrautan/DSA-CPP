5ad04a40bd5cc59e3226f96e02b6b787e38d5019
nikhilrautan <nikhilrautan1@gmail.com>
2026-04-08 12:02:23 +0530

    Added Code

---
+++ 10.QUEUE/02 Questions/print_elements.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+// print elements withouut removing 
+int main()
+{
+    queue<int> q;
+    q.push(5);
+    q.push(15);
+    q.push(25);
+    q.push(35);
+    q.push(45);
+
+    //value print karao
+    int n = q.size();
+
+    while(n--)
+    {
+        cout<<q.front()<<" ";
+        q.push(q.front());
+        q.pop();
+    }
+
+    // ab elements present hai ki nhii ? dekhne k liye size print krado
+    cout<<q.size()<<endl;
+}
