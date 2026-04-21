b62a86c98b8f3c2f666d7ccd53f2b99fb1f72ecb
nikhilrautan <nikhilrautan1@gmail.com>
2026-04-21 22:10:21 +0530

    UPDATED

--- 01.ARRAYS/02_Questions of Arrays/Reverse_an_array.cpp
+++ 01.ARRAYS/02_Questions of Arrays/Reverse_an_array.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 int main()
 {
-    int arr[6]={1,2,3,4,5,6};
-    int start = 0 , end =5;
-    while(start<end)
-    {
-        swap(arr[start],arr[end]);
+     int arr[6]={1,2,3,4,5,6};
+     int start =0 , end=5;
+     while(start<=end)
+     {
+        swap(arr[start], arr[end]); // swap krte jaenge jb tk wo while ki condition ko statisfy krta hai
         start++;
-        end --;
-    };
-    for(int i=0;i<6;i++)
-    {
-       cout<<arr[i]<<" ";
-    }
+        end--;
+     };
+     for(int i=0;i<6;i++)
+     {// print kr denge
+        cout<<arr[i]<<" ";
+     }
 }
