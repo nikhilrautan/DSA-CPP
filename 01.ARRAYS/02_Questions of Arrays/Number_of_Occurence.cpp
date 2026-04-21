c2529b36b16cd446068c4bfdcad307067a51f4d1
nikhilrautan <nikhilrautan1@gmail.com>
2026-04-21 22:02:52 +0530

    UPDATED

--- 01.ARRAYS/02_Questions of Arrays/Number_of_Occurence.cpp
+++ 01.ARRAYS/02_Questions of Arrays/Number_of_Occurence.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int count(int arr[], int n, int x)
 {
-    int cnt = 0;   // renamed to avoid confusion
-    for (int i = 0; i < n; i++)
+    int cnt = 0; // renamed to avoid confusion
+        for (int i = 0; i < n; i++)
     {
         if (arr[i] == x)
         {
@@ -13,12 +13,10 @@ int count(int arr[], int n, int x)
     }
     return cnt;
 }
-
 int main()
 {
-    int arr[] = {1, 2, 2, 2, 3, 4};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int x = 2;
-
-    cout << "Count = " << count(arr, n, x);
+    int arr[]= {1,2,2,2,3,4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int x=2;
+    cout<<"Count =" << count(arr,n,x);
 }
