9d27598fe71124b18f320b7eb1d4cc865414e0d0
nikhilrautan <nikhilrautan1@gmail.com>
2026-04-21 22:04:44 +0530

    UPDATED

--- 01.ARRAYS/02_Questions of Arrays/Number_of_Occurence.cpp
+++ 01.ARRAYS/02_Questions of Arrays/Number_of_Occurence.cpp
@@ -6,7 +6,7 @@ int count(int arr[], int n, int x)
     int cnt = 0; // renamed to avoid confusion
         for (int i = 0; i < n; i++)
     {
-        if (arr[i] == x)
+        if (arr[i] == x) // agr match krta hai to count ki value bdha do
         {
             cnt++;
         }
@@ -16,7 +16,7 @@ int count(int arr[], int n, int x)
 int main()
 {
     int arr[]= {1,2,2,2,3,4};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int n = sizeof(arr)/sizeof(arr[0]); // no of elements nikalnae k liye
     int x=2;
     cout<<"Count =" << count(arr,n,x);
 }
