7e8ae39e29b8f68ab14eb78750c73d5c8aca6573
nikhilrautan <nikhilrautan1@gmail.com>
2026-04-25 13:48:07 +0530

    UPDATED

---
+++ 06.RECURSION/02 Basic Questions/04_n_to_1_even.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+// print n to 1(reverse order)
+void print(int n)
+{
+    if(n==2)
+    {
+        cout<<2;
+        return;
+    }
+    cout<<n<<endl;
+    print(n-2);
+}
+int main()
+{
+    int n=10;
+    print(n);
+}
