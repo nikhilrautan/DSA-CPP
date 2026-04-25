ac8a7c774243794175456f5ced42fe5a6184a893
nikhilrautan <nikhilrautan1@gmail.com>
2026-04-25 15:33:52 +0530

    UPDATED

--- Basic/Recursion/frog1.cpp
+++ Basic/Recursion/frog1.cpp
@@ -1,21 +1,31 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+
 int frog(int index,vector<int>&a, vector<int>&dp)
-{   if(dp[index]!=-1) return dp[index]; 
-    if(index==0) dp[index]=0;
+{
+    if(dp[index]!=-1) return dp[index]; 
+
+    if(index==0) return dp[index]=0;  
     if(index==1) return dp[index]= abs(a[0]-a[1]);
 
-    int cost1 = frog(index-1,a,dp) +abs(a[index] -a[index-1]);
-    int cost2 = frog(index-2,a,dp) +abs(a[index] -a[index-2]);
+    int cost1 = frog(index-1,a,dp) + abs(a[index] - a[index-1]);
+    int cost2 = frog(index-2,a,dp) + abs(a[index] - a[index-2]);
+
     return dp[index]= min(cost1,cost2);
 }
+
 int main()
-{    int n;
-    cin>>n;
+{
+    int n;
+    cin >> n;
+
     vector<int>a(n);
-    for(auto &i:a) cin>>i;
+    for(auto &i:a) cin >> i;
+
     vector<int>dp(n,-1);
-    cout<<frog(n-1,a,dp);
+
+    cout << frog(n-1,a,dp);
+
     return 0;
 }
--- Basic/Recursion/frog2.cpp
+++ Basic/Recursion/frog2.cpp
@@ -1,22 +1,36 @@
 #include<iostream>
+#include<bits/stdc++.h>
 #include<vector>
 using namespace std;
-int frog(int index,vector<int>&a, vector<int>&dp)
-{   if(dp[index]!=-1) return dp[index]; 
-    if(index==0) dp[index]=0;
-    if(index==1) return dp[index]= abs(a[0]-a[1]);
 
-    int cost1 = frog(index-1,a,dp) +abs(a[index] -a[index-1]);
-    int cost2 = frog(index-2,a,dp) +abs(a[index] -a[index-2]);
+int frog(int index,int k,vector<int>&a, vector<int>&dp)
+{
+    if(dp[index]!=-1) return dp[index]; 
 
-    return dp[index]= min(cost1,cost2);
+    if(index==0) return dp[index]=0;
+    if(index==1) return dp[index]= abs(a[1]-a[0]);
+
+    int mini = INT_MAX;
+
+    for(int i=index-1;i>=max(0,index-k);i--)
+    {
+        mini = min(mini, frog(i,k,a,dp) + abs(a[index]-a[i]));
+    }
+
+    return dp[index]= mini;
 }
+
 int main()
-{    int n;
-    cin>>n;
+{
+    int n, k;
+    cin >> n >> k;   
+
     vector<int>a(n);
-    for(auto &i:a) cin>>i;
+    for(auto &i:a) cin >> i;
+
     vector<int>dp(n,-1);
-    cout<<frog(n-1,a,dp);
+
+    cout << frog(n-1,k,a,dp);
+
     return 0;
 }
