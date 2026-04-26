d0532716c3f4deb8dfd91028da4158f43d3acba3
nikhilrautan <nikhilrautan1@gmail.com>
2026-04-26 10:23:18 +0530

    UPDATED

--- 06.RECURSION/03 Recursion in ARRAYS/04_Printing_Array.cpp
+++ 06.RECURSION/03 Recursion in ARRAYS/04_Printing_Array.cpp
@@ -1,39 +1,18 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-//forward Printing
+
 void print(int arr[], int index, int n)
 {
-    if (index == n)
+    if(index == n)
+    {
         return;
-
+    }
     cout << arr[index] << " ";
     print(arr, index + 1, n);
 }
 
 int main()
 {
-    int arr[] = {3, 4, 5, 6, 7};
-    int n = 5;
-
-    print(arr, 0, n);
-    return 0;
-}
-
-// reverse Printing
-void print2(int arr[], int index)
-{
-    if (index == -1)
-        return;
-
-    cout << arr[index] << " ";
-    print2(arr, index - 1);
-}
-
-int main()
-{
-    int arr[] = {3, 4, 5, 6, 7};
-    int n = 5;
-
-    print2(arr, n - 1);
-    return 0;
+    int arr[] = {3, 7, 6, 2, 8};
+    print(arr, 0, 5);
 }
