ce4bdf089ed602e1f71aaf1e96690f966ef2696f
nikhilrautan <nikhilrautan1@gmail.com>
2026-04-28 20:31:10 +0530

    UPDATED

--- 06.RECURSION/09 Recursion on Subset Sum/02_Target_sum.cpp
+++ 06.RECURSION/09 Recursion on Subset Sum/02_Target_sum.cpp
@@ -7,23 +7,19 @@ bool findTarget(int arr[], int index, int n, int target)
     // If target becomes 0, we found a valid subset
     if (target == 0)
         return true;
-
     // If we reach end or target becomes negative
     if (index == n || target < 0)
         return false;
-
     // Either skip current element
     // OR include current element
     return findTarget(arr, index + 1, n, target) ||
            findTarget(arr, index + 1, n, target - arr[index]);
 }
-
 int main()
 {
     int arr[] = {1, 2, 3, 4};
     int n = 4;
     int target = 6;
-
     if (findTarget(arr, 0, n, target))
         cout << "Subset exists";
     else
