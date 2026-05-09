b6a7382dcdbbe6bba12ca75f844e0f2f132f7331
nikhilrautan <nikhilrautan1@gmail.com>
2026-05-09 22:46:57 +0530

    UPDATED

---
+++ 11.TREES/01BINARY_TREE/Traversal/Preorder.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *left, *right;
+    Node(int value)
+    {
+        left = right = NULL;
+        data = value;
+    }
+};
+void Preorder(Node *root)
+{
+   if(root ==NULL) return;
+    cout<<root->data;
+    Preorder(root->left);
+    Preorder(root->right);
+}
+int main()
+{
+    
+}
