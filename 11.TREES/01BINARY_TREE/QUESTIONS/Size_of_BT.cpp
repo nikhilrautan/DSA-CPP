4f01dc25a726f27f8aaad7b669e7c5f3de4dddbd
nikhilrautan <nikhilrautan1@gmail.com>
2026-05-14 20:29:57 +0530

    UPDATED

--- 11.TREES/01BINARY_TREE/QUESTIONS/Size_of_BT.cpp
+++ 11.TREES/01BINARY_TREE/QUESTIONS/Size_of_BT.cpp
@@ -2,17 +2,29 @@
 #include<vector>
 #include<queue>
 using namespace std;
-
-// Node of binary tree 
-class Node {
-public:
+struct Node {
     int data;
-    Node* left;
-    Node* right;
+    struct Node* left;
+    struct Node* right;
 
     Node(int val) {
         data = val;
-        left = NULL;
-        right = NULL;
+        left = right = NULL;
+    }
+};
+
+
+class Solution {
+public:
+
+    // Function to count total nodes
+    int getSize(Node* root) {
+
+        // if tree is empty
+        if(root == NULL)
+            return 0;
+
+        // count current node + left subtree + right subtree
+        return 1 + getSize(root->left) + getSize(root->right);
     }
 };
