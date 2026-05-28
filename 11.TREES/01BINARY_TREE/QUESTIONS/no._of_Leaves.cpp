b3293ac0b93df386e4369295326e85e5d453fe96
nikhilrautan <nikhilrautan1@gmail.com>
2026-05-28 10:27:19 +0530

    UPDATED

--- 11.TREES/01BINARY_TREE/QUESTIONS/no._of_Leaves.cpp
+++ 11.TREES/01BINARY_TREE/QUESTIONS/no._of_Leaves.cpp
@@ -1,3 +1,15 @@
 #include<iostream>
 using namespace std;
 
+struct Node {
+
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+
+        data = val;
+        left = right = NULL;
+    }
+};
