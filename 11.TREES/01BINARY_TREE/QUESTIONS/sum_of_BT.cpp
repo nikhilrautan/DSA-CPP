38b8819859a8e80c7564fb61fc56b97d7ad399e1
nikhilrautan <nikhilrautan1@gmail.com>
2026-05-28 10:09:01 +0530

    Fixed Errors

--- 11.TREES/01BINARY_TREE/QUESTIONS/sum_of_BT.cpp
+++ 11.TREES/01BINARY_TREE/QUESTIONS/sum_of_BT.cpp
@@ -1,19 +1,41 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-// Node of binary tree 
-class Node {
+// Node of binary tree
+class Node
+{
 public:
     int data;
-    Node* left;
-    Node* right;
-    Node(int val) {
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
         data = val;
         left = NULL;
         right = NULL;
     }
 };
-void Total(Node* root,int &sum)
+void Total(Node *root, int &sum)
 {
-    if(root==NULL)
+    if (root == NULL) return;
+    sum += root->data;
+    Total(root->left,sum);
+    Total(root->right,sum);
+}
+
+int main()
+{
+    // Creating tree
+    Node* root = new Node(1);
+
+    root->left = new Node(2);
+    root->right = new Node(3);
+    int sum = 0;
+
+    Total(root, sum);
+
+    cout << "Total Sum = " << sum;
+
+    return 0;
+    
 }
