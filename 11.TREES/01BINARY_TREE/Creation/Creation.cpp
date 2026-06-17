6f8de7f533c1211ce984481b2c0fd5552e94caf2
nikhilrautan <nikhilrautan1@gmail.com>
2026-06-17 23:45:38 +0530

    Update TREE's Creation  folder

--- 11.TREES/01BINARY_TREE/Creation/Creation.cpp
+++ 11.TREES/01BINARY_TREE/Creation/Creation.cpp
@@ -23,7 +23,6 @@ Node* BinaryTree()
         return NULL;
 
     Node* temp = new Node(x);
-
     // Left side creation
     cout << "Enter the left child of " << x << ": ";
     temp->left = BinaryTree();
